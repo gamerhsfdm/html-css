750f5e84611d95ab4240292e353de487726f5272
gamerhsfdm <76122031+gamerhsfdm@users.noreply.github.com>
2026-05-28 17:49:10 -0300

    responsividade telas maiores

--- desafios/d-016-portfolio/scripts/script.js
+++ desafios/d-016-portfolio/scripts/script.js
@@ -9,10 +9,10 @@ temaEscuro(temasalvo === 'escuro');
 function temaEscuro(tipo) {
   if (tipo == true) {
     body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    botao.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
   } else {
     body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    botao.innerHTML = '<i class="bi bi-moon-fill"></i>';
   }
 }
 
